71e99c8f32b5893c5b2bcb30bdb62534e8dafc8c
MuhammadAryatamaM <muhammad.aryatama2006@gmail.com>
2026-04-27 09:40:50 +0700

    menambah custom action (status change)

--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -2,11 +2,13 @@
 
 namespace App\Filament\Resources\Posts\Tables;
 
+use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ReplicateAction;
+use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\DatePicker;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\ColorColumn;
@@ -81,7 +83,16 @@ class PostsTable
             ->recordActions([
                 ReplicateAction::make(),                
                 EditAction::make(),
-                DeleteAction::make()
+                DeleteAction::make(),
+                Action::make('status')
+                    ->label('Status Change')
+                    ->icon('heroicon-o-check-circle')
+                    ->schema([
+                        Checkbox::make('published')->default(fn ($record) : bool => $record->published),
+                    ])
+                    ->action(function ($record, $data) {
+                        $record->update(['published' => $data['published']]);
+                    })
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
